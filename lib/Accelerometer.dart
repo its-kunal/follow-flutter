99914bf41b61ea68c13afd1b96189eb2c0a3572f
kunal <kunal.py3@gmail.com>
2022-07-29 07:27:33 +0530

    commit 4

---
+++ lib/Accelerometer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UserAc extends StatefulWidget {
+  const UserAc({Key? key}) : super(key: key);
+
+
+  @override
+  State<UserAc> createState() => _UserAcState();
+}
+
+class _UserAcState extends State<UserAc> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 0,
+      width: 0,
+    );
+  }
+}
--- lib/algoToSend.dart
+++ lib/algoToSend.dart
@@ -1,3 +1,19 @@
 // Input will be a list, and calculate max of accelerometer in all directions
 // in  time period of 500ms
 // Output will be a maximum of all numbers and with a symbol.
+
+import 'dart:async';
+
+import 'package:sensors_plus/sensors_plus.dart';
+
+void main() {
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+  List<double>? _userAccelerometer;
+  List<double>? _gryoMeter;
+  _streamSubscriptions.add(userAccelerometerEvents.listen((event) {
+    _userAccelerometer = <double>[event.x, event.y, event.z];
+  }));
+  
+}
+
+
